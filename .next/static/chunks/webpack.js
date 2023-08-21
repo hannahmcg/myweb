4d1f532b885ddac4ad7c63d8d0740cf5a91cdbed
test <test@macbook-pro-2.lan>
2023-08-20 21:27:18 -0700

    update work card titles and update compass and cssa case studies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a563c1ad2616e45"; }
+/******/ 		__webpack_require__.h = function() { return "87ffa119ecdf699c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
