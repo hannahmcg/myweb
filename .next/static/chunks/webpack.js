1499ad1aa4f712db057dceb7a186ffcf4cec69b0
test <test@macbook-pro-2.lan>
2023-11-29 15:33:06 -0800

    fix paragraph spacing on cards

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ebce5b78e0165f83"; }
+/******/ 		__webpack_require__.h = function() { return "27a02c9a9f8029a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
