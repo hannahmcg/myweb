0f329728bd0b7c998d5b55caf4322cdf8da2d30a
test <test@macbook-pro-3.lan>
2024-02-05 23:25:27 -0800

    add desktop breakpoint and adjust navbar textsize

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fee2303621831c04"; }
+/******/ 		__webpack_require__.h = function() { return "ff0a86a08d3f19c4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
