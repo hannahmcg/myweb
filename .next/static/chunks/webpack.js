08995c9112d2ab4af56e941d2f141775c940f128
test <test@macbook-pro-3.lan>
2024-02-26 02:42:22 -0800

    change and symbol to plus symbol for cohesive design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ff0a86a08d3f19c4"; }
+/******/ 		__webpack_require__.h = function() { return "aa82a2ab28416514"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
